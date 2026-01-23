41feaa2826558f26b53ee23592ae3f6a9015b1ec
Indy Lyness <indy.lyness@gmail.com>
2026-01-23 13:38:06 -0600

    Added function to load dice plus some refactoring/documentation goblining

--- test_emerson.py
+++ test_emerson.py
@@ -2,35 +2,79 @@ import pandas as pd
 import random
 import nltk
 from collections import Counter
+import csv
 
 def load_words():
     """
     Loads nltk words that are valid for Q-less
     """
-    nltk.download('words')
-    from nltk.corpus import words
-    return [w for w in words.words() if w.islower() and len(w) >= 3 and w.isalpha()]
+    try:
+        from nltk.corpus import words
+    except:
+        nltk.download('words')
+        from nltk.corpus import words
+    return [w for w in words.words() if w.islower() and len(w) >= 3 and len(w) <= 12 and w.isalpha()]
 
-
-def roll(dice_csv:str):
+def load_dice() -> list[list[str]]:
     """
-    Picks 12 random letters from the Q-less dice
+    Loads the Q-less dice.
+
+    Returns
+    -------
+    list of list of str
+        The possible letters for each die.
     """
-    dice = pd.read_csv(dice_csv, header=None)
-    return [random.choice(row).lower() for _, row in dice.iterrows()]
+    with open('dice.csv', newline='') as f:
+        return [list(map(str.lower, row)) for row in csv.reader(f, delimiter=',', quotechar='|')]
 
-if __name__ == '__main__':
-    letters = roll('./dice.csv')
-    words = load_words()
-    print(f'Rolled letters: {letters}')
-    print(f'Total words: {len(words)}')
+def roll(dice: list[list[str]]) -> list[str]:
+    """
+    Picks 12 random letters from the Q-less dice.
 
-    letters_count = Counter(letters)
+    Parameters
+    ----------
+    dice : list of list of str
+        The dice to roll.
+
+    Returns
+    -------
+    list of str
+        The resulting roll.
+    """
+    return [random.choice(die) for die in dice]
+
+def get_valid_words(roll: list[str], words: list[str]) -> list[str]:
+    """
+    Get all valid words for a roll.
+
+    Parameters
+    ----------
+    roll : list of str
+        The letters in the roll.
+    words : list of str
+        The pool of words to consider.
+
+    Returns
+    -------
+    list of str
+        All possible words in Q-less.
+    """
+    letters_count = Counter(roll)
     valid_words = []
     for word in words:
         word_count = Counter(word)
         if word_count <= letters_count:
             valid_words.append(word)
+    return valid_words
+
+if __name__ == '__main__':
+    dice = load_dice()
+    letters = roll(dice)
+    words = load_words()
+    print(f'Rolled letters: {letters}')
+    print(f'Total words: {len(words)}')
+
+    valid_words = get_valid_words(letters, words)
 
     print(f'Found {len(valid_words)} valid starting words')
     lengths = [len(w) for w in valid_words]
