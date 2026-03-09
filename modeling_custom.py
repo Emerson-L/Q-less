3353ef5d205d1de65446e201585389eabb74058b
Indy Lyness <indy.lyness@gmail.com>
2026-03-09 10:41:59 -0500

    Started implementing layer architecture

--- modeling_custom.py
+++ modeling_custom.py
@@ -23,3 +23,51 @@ class Net():
         
     def forward_pass(self, x:np.ndarray):
         pass
+
+class Layer:
+    """
+    One layer of a neural network.
+
+    Supports forward and backward passes to calculate weights.
+
+    Attributes
+    ----------
+    weights : np.array
+        The weights of the layer.
+    bias : float
+        The bias of the layer.
+    """
+    
+    def __init__(self):
+        pass
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        """
+        Compute the forward pass for a given input.
+
+        Parameters
+        ----------
+        input : np.array
+            The potentially multi-dimensional input to feed into the layer.
+
+        Returns
+        -------
+        np.array
+            The result of passing input through the layer.
+        """
+        pass
+
+    def backward(self) -> None:
+        pass
+
+class Linear(Layer):
+    def __init__(self, layer_size: int):
+        self.weights = np.random.randn(layer_size)
+        self.bias = np.random.rand()
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        self.activations = input @ self.weights + self.bias
+        return self.activations
+    
+    def backward(self, loss: np.ndarray, input: np.ndarray) -> np.ndarray:
+        pass 
