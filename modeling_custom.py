a14d512cac4779b242ca0cf384f5d15a3db30202
Jemersonn <106408331+Jemersonn@users.noreply.github.com>
2026-03-09 09:59:03 -0500

    Some skeleton of custom_modeling

--- modeling_custom.py
+++ modeling_custom.py
@@ -1,3 +1,25 @@
 import numpy as np
 
 #make cnn
+
+class Net():
+    """
+    Custom neural network architecture that supports convolutional layers
+    """
+    def __init__(self):
+        pass
+
+    def convolutional(self, x:np.ndarray, size:int, stride:int, padding:int):
+        pass
+
+    def fully_connected(self, x:np.ndarray, in_size:int, out_size:int):
+        pass
+
+    def relu(self, x:np.ndarray):
+        pass
+
+    def maxpool(self, x:np.ndarray):
+        pass
+        
+    def forward_pass(self, x:np.ndarray):
+        pass
